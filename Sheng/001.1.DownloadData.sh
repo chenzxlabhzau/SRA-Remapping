e6044438ecd7fe86d77d60c3063f0030e963583b
Sheng-Hu Qian <qians@webmail.hzau.edu.cn>
2020-11-20 17:22:06 +0800

    Create 001.1.DownloadData.sh

---
+++ Sheng/001.1.DownloadData.sh
@@ -0,0 +1,6 @@
+#Data download
+cd ~/SRAremapping/Input/SeqData/GSE117217
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE117nnn/GSE117217/suppl/GSE117217_supplemental_metadata.tsv.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE117nnn/GSE117217/suppl/GSE117217_intergenic_counts.tsv.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE117nnn/GSE117217/suppl/GSE117217_gene_counts.tsv.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE117nnn/GSE117217/suppl/GSE117217_dmel_r6-11.intergenic.gtf.gz
